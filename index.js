7fe3394c7c94fa3bf8b5d02c38f9f4b918636745
Jéssica Moreira <95175671+jesmoreira@users.noreply.github.com>
2022-01-11 17:22:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//variaveis (quando a variavel pode mudar)
+/*let idade = 25;
+let nome = 'Jéssica';
+console.log(nome, idade);*/
+
+//constantes (quando a variavel não muda)
+/*const ingressoValorAdulto = 20;
+console.log(20);*/
+
+//tipos primitivos
+/*let nome = 'Jéssica'; //string literal
+let idade = 25; // number literal
+let estarAprovado = true; // boolean
+let sobrenome = undefined; // undefined
+let corSelecionada = null; //redefinir um valor */
+
+
+
